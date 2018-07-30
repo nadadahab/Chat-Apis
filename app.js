1266e46cdccf214cbe9a7efdd09a0c4f3154d3d0
nadadahab <nadadahab906@gmail.com>
2018-07-30 16:43:24 +0200

    Edit app.js

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors=require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var messagesRouter=require('./routes/messages');
+
 
 var app = express();
 
@@ -18,7 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
+app.use('/Tasks',messagesRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
